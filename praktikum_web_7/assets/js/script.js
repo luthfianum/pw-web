558e84e46e41c0a92a61d53ce2a38fdc9cbc6254
Luthfi Anum Pratama <luthfianump@gmail.com>
2021-12-22 19:41:56 +0800

    refactor code

--- praktikum_web_7/assets/js/script.js
+++ praktikum_web_7/assets/js/script.js
@@ -1,7 +1,11 @@
+// Siapkan element-element yang akan digunakan
 const select_provinsi = document.querySelector('#select_provinsi');
 const select_kota = document.querySelector('#select_kota');
 const toggle = document.querySelector('#toggle');
 const result = document.querySelector('#result');
+const html = document.querySelector('html');
+
+// Siapkan Data Kota. biar enak berupa html aja jadi tinggal inner html
 const data = {
   'Kalimantan Timur' : `
     <option disabled="disabled" selected="selected">Kota</option>
@@ -39,30 +43,37 @@ const data = {
 }
 
 const func_select_provinsi = () => {
+  // Ambil Nilai provinsi
   let provinsi_value = select_provinsi.value;
+
+  // Enable Input Kota 
   select_kota.removeAttribute('disabled');
+
+  // Masukkan html di data kedalam input kota
   select_kota.innerHTML = data[provinsi_value];
+
+  // Ganti Tulisan Menjadi Input Kota
   result.innerText = `Silahkan Pilih Kota`;
 }
 
 const func_select_kota = () => {
+  // Ambil value provinsi dan kota
   let provinsi_value = select_provinsi.value;
   let kota_value = select_kota.value;
+
+  // Tampilkan pada result
   result.innerText = `pada Provinsi ${provinsi_value} ada Kota ${kota_value}`;
 }
 
 const func_toggle = () => {
-  let html = document.querySelector('html');
+  // Ambil Nilai Apakah Toggle True(checked) atau False(unchecked)
   let toggle_checked = toggle.checked;
 
-  if(toggle_checked){
-    html.setAttribute('data-theme','dark')
-  }else{
-    html.setAttribute('data-theme','light')
-  }
-  console.log(toggle_checked);
+  // Jika Value toggle true, ubah jadi dark mode. jika false ubah jadi light mode.
+  toggle_checked ? html.setAttribute('data-theme','dark') : html.setAttribute('data-theme','light')
 }
 
+// Ini daftar event listener-nya
 select_provinsi.addEventListener('change', func_select_provinsi);
 select_kota.addEventListener('change', func_select_kota);
 toggle.addEventListener('click', func_toggle)
